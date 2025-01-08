e9114a34aa4877630c6fd8cf2041f9a41e7f232d
SimoneTaccarelli <simone-taccarelli@hotmail.it>
2025-01-08 19:18:27 +0100

    aggiornamento immagine

--- ScriptArtista.js
+++ ScriptArtista.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+function getArtistInfo(){
     let url = new URL(window.location.href);
     let id = url.searchParams.get("id");
     console.log(id);
@@ -7,26 +7,41 @@ document.addEventListener("DOMContentLoaded", function(){
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        let url = data.tracklist;
-        getPopularSongs(url);
-    })
-    .catch(error => console.log(error));
-});
-
-function getPopularSongs(url){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        let popularSongs = data.data;
-        popularSongs.sort((a, b) => b.rank - a.rank);
-        
-        let popularSongsContainer = document.getElementById("popular-songsST");
-        popularSongs.forEach(song => {
-            popularSongsContainer.innerHTML += `<li>${song.title} ${song.rank} ${song.duration}</li>`;
-        });
-        
+        let tracklistUrl = data.tracklist;
+        getPopularSongs(tracklistUrl);
+        let imgContainer = document.getElementById("first-container-ab");
+        imgContainer.style.backgroundImage = `url(${data.picture_xl})`;
+        imgContainer.style.backgroundSize = 'cover';
+        imgContainer.style.backgroundPosition = 'center';
+        let nomeArtista = document.getElementById("nomeArtista-ab");
+        nomeArtista.innerHTML = data.name;
     })
     .catch(error => console.log(error));
 }
+
+async function getPopularSongs(tracklistUrl){
+    try{
+        let response = await fetch(tracklistUrl);
+        let data = await response.json();
+        let popularSongs = data.data;
+        popularSongs.sort((a, b) => b.rank - a.rank);
+        
+        let popularSongsContainer = document.getElementById("canzoni-popolari");
+        popularSongsContainer.innerHTML = "<table class='table table-striped'> <thead><tr><th>Titolo</th><th>Rank</th><th>Durata</th></tr></thead><tbody>";
+        popularSongs.forEach(song => {
+            if(song.rank <= 5){ 
+                popularSongsContainer.innerHTML += `<tr><td>${song.title}</td><td>${song.rank}</td><td>${song.duration}</td></tr>`;
+            }
+        });
+        popularSongsContainer.innerHTML += "</tbody></table>";
+    }catch(error){
+        console.log(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    getArtistInfo();
+    getPopularSongs();
+});
 
 
